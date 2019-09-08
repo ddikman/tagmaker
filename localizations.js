c65faeb3c44a9219dda3508be213786f85d5c784
David <ddikman@gmail.com>
2019-09-08 23:51:14 +0200

    Added translation

---
+++ localizations.js
@@ -0,0 +1,29 @@
+const locale = {
+    'sv': {
+        'download-svg': 'Ladda ner SVG',
+        'upload-hint': 'Släpp en fill här eller tryck för att ladda upp'
+    },
+}
+
+locale['sv-SE'] = locale.sv;
+
+function currentLocale() {
+    return locale[navigator.language];
+}
+
+function translationAvailable() {
+    return currentLocale() !== undefined;
+}
+
+$(document).ready(function() {
+    // current locale has translation
+    if (translationAvailable()) {
+        $('[data-translate]').each(function(_, item) {
+            item = $(item);
+            const term = item.attr('data-translate');
+            const translation = currentLocale()[term];
+            console.log(`setting translation for term ${term} to: ${translation}`);
+            item.text(translation);
+        });
+    }
+})
